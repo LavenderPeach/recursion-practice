a4e205541a5900cc21cc41a29c6d6bef89d41264
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-09-07 14:54:27 -0400

    Write a recursion for the fibonacci sequence
    A similar structure at first to the iteration fibonaccci script. This time
    I used a ternary operator to determine if a situation was true or false.
    Once we know the user did not input a number => 2, there is a spread operator
    that takes the array from n-1 and adds onto it by taking the second to
    last and third to last integers.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function fibs(n) {
-    const arr = [0,1];
-    if (n<=0) return "Please enter a valid number."
-    if (n === 1) return [0];
-    if (n === 2) return [arr];
-    for (let i = 2; i < n; i++)
-        arr.push(arr[arr.length-2]+arr[arr.length-1]);
-    return arr;
+function fibsRec(n) {
+    return n<=0 ? "Please enter a valid number"
+    : n === 1
+    ? [0]
+    : n === 2
+    ? [0,1]
+    :[...fibsRec(n-1), fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]];
 }
 
-console.log(fibs(8));
+console.log(fibsRec(8));
